7a38069a827cbb6ac77cd814f4e6b171e1755477
Sarah.Keller <sarah29.84@gmail.com>
2024-04-17 20:21:41 +0200

    Fetched courses

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
-fetch('https://catfact.ninja/fact')
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  });
-
 function App() {
-  fetch('https://catfact.ninja/fact')
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
+  const [courses, setCourses] = useState([]);
+
+  const fetchCourses = () => {
+    fetch('https://private-e05942-courses22.apiary-mock.com/courses')
+      .then((res) => res.json())
+      .then((data) => {
+        const resData = data.map((item) => {
+          return {
+            title: item.title,
+            url: item.url,
+            startDate: item.next_start_formatted,
+          };
+        });
+        setCourses(resData);
+      });
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+
   return (
     <div className="App">
-      <button>Generate cat fact</button>
+      <button onClick={fetchCourses}>Generate cat fact</button>
+      <>
+        {courses.map((course, index) => (
+          <div key={index}>
+            <p>{course.title}</p>
+            <p>{course.url}</p>
+            <p>{course.startDate}</p>
+          </div>
+        ))}
+      </>
     </div>
   );
 }
